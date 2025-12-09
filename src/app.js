ba8f4ab7945d4529116f243fd03bab2bc9a0e5a5
nam-singh <nam.singh@ibm.com>
2025-12-09 17:51:13 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express=require('express');
 const app=express();
 
-app.use("/test",(req, res) => { res.send('Hello World from test!'); });
-
+app.get('/user',(req,res)=>{
+    res.send({firstname:'Namrata',lastname:'Singh'});
+});
+app.post('/user',(req,res)=>{
+    res.send('Post request is successful');
+});
+app.delete('/user',(req,res)=>{
+    res.send('Delete request is successful');
+});
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
